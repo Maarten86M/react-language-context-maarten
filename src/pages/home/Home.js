8b3e4b2033e0b5e13452ae803444f28b5f515d8f
Maarten86M <maartenmoelands@gmail.com>
2021-06-16 16:16:51 +0200

    eind van de opdracht

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import content from '../../data/content.json';
+import {useLanguageContext} from "../../context/LanguageContext";
 
 function Home() {
-  return (
-    <div className="page-container">
-      <h2>{content.nl.home.title}</h2>
-      <p>{content.nl.home.introText}</p>
-    </div>
-  );
+    const {language} = useLanguageContext();
+    return (
+        <div className="page-container">
+            {language === 'nl' && (<h2>{content.nl.home.title}</h2>)}
+            {language === 'es' && (<h2>{content.es.home.title}</h2>)}
+
+            {language === 'nl' && (<p>{content.nl.home.introText}</p>)}
+            {language === 'es' && (<p>{content.es.home.introText}</p>)}
+        </div>
+    );
 }
 
 export default Home;
